18a1a07e1b24564733c3137472b628fc9e69bb87
Adedunmol <oyewaleadedunmola@gmail.com>
2021-04-23 11:59:01 +0000

    First commit

---
+++ website/home.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, render_template, request, send_file, redirect, url_for
+from website import qrcode
+
+home = Blueprint('home', __name__)
+
+
+@home.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+@home.route('/qrcode', methods=['GET', 'POST'])
+def get_qrcode():
+    if request.method == 'POST':
+        data = request.form.get('url')
+    return render_template('code.html', data=data)
